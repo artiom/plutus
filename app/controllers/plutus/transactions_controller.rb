e2f19452b219d351246e043d59fe6fe340ebccd4
Artiom Lunev <artiom.lunev@gmail.com>
2013-05-02 15:19:41 +0300

    needed only for engine development, fixed circular dependency in rails4 with ruby2

--- app/controllers/plutus/transactions_controller.rb
+++ app/controllers/plutus/transactions_controller.rb
@@ -11,7 +11,6 @@ module Plutus
   #
   # @author Michael Bulat
   class TransactionsController < ApplicationController
-    unloadable
     # @example
     #   GET /transactions
     #   GET /transactions.xml
